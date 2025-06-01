2ceaae71510c0c8b5d54b77ce548084a4fc349a0
rajurao <rajurao1107@gmail.com>
2025-06-01 22:26:44 +0530

    commit

--- src/app/services/body/tummy-tuck-(abdominoplasty)/Knowledge.jsx
+++ src/app/services/body/tummy-tuck-(abdominoplasty)/Knowledge.jsx
@@ -17,11 +17,11 @@ const Knowledge = () => {
     <section>
       <div className="w-full flex flex-col items-center justify-center py-8">
         <div className="flex w-full max-lg:flex-col-reverse gap-10 max-w-[1300px] max-lg:justify-center max-lg:items-center">
-          <div className="w-[28%]  max-lg:w-[90%] h-[28rem] sticky top-[5rem] mr-8 rounded-lg overflow-hidden shadow-lg">
+          <div className="w-[28%] max-lg:hidden  max-lg:w-[90%] h-[28rem] sticky top-[5rem] mr-8 rounded-lg overflow-hidden shadow-lg">
             {/* Header */}
             <div className="bg-[#1AAEBC] py-4 px-6">
               <h1 className="text-white text-center font-bold text-2xl">
-                CONSULT US
+                BOOK CONSULTATION
               </h1>
               <form className="space-y-4 py-8">
             <div className="grid md:grid-cols-2 gap-4">
