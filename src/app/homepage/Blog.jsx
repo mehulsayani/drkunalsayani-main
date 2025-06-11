e3a21ee1e5dff1c71b7ec885219d92c5572bb7bb
rajurao <rajurao1107@gmail.com>
2025-06-11 17:11:14 +0530

    commit

--- src/app/homepage/Blog.jsx
+++ src/app/homepage/Blog.jsx
@@ -85,14 +85,16 @@ export default function Blog() {
             </div>
           ))}
         </div>
-        <button
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
-          className="bg-[#1BA9B5] flex justify-center mt-8 items-center gap-2 hover:bg-[#148d98] transition-colors
+        <a href="/blog">
+          <button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            className="bg-[#1BA9B5] flex justify-center mt-8 items-center gap-2 hover:bg-[#148d98] transition-colors
                      text-white font-semibold px-8 py-2 rounded-md shadow-md"
-        >
-          View All
-        </button>
+          >
+            View All
+          </button>
+        </a>
       </div>
     </section>
   );
