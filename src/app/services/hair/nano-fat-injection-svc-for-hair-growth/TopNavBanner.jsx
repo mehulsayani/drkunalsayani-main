a2b755c9984513f563bad26a0e1bb35e67e38909
rajurao <rajurao1107@gmail.com>
2025-06-12 11:47:28 +0530

    commit

--- src/app/services/hair/nano-fat-injection-svc-for-hair-growth/TopNavBanner.jsx
+++ src/app/services/hair/nano-fat-injection-svc-for-hair-growth/TopNavBanner.jsx
@@ -12,7 +12,7 @@ export default function TopNavBanner() {
         fill
         className="w-full object-cover -z-20"
         alt=""
-        src={"/images/services/hair.jpg"}
+        src={"/images/services/hair2.jpg"}
       />
 
       {/* Optional decorative elements */}
