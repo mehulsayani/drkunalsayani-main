bf12ad37a93688802ba5f8702885b161df67e273
rajurao <rajurao1107@gmail.com>
2025-06-12 10:26:08 +0530

    commit

--- src/app/services/hair/hair-transplant/Overview.jsx
+++ src/app/services/hair/hair-transplant/Overview.jsx
@@ -14,42 +14,25 @@ export default function Overview() {
         />
        <div className="text-gray-600 leading-relaxed text-base max-lg:text-base">
          <p className="mb-3">
-          Dreaming of a smoother, firmer abdomen? Do you find excess skin and
-          stubborn fat deposits around your midsection resistant to diet and
-          exercise? A tummy tuck, also known as abdominoplasty, is a
-          transformative surgical procedure designed to address these concerns,
-          creating a flatter and more toned abdominal profile.
+        Are you experiencing hair thinning, a receding hairline, or bald spots that affect your selfesteem? Hair loss can be a deeply personal and often distressing experience, but it doesn't have
+to be permanent. At Dr. Kunal Sayani's state-of-the-art aesthetic clinic, we offer advanced hair
+transplant solutions designed to restore your natural hair growth and help you regain a fuller,
+more youthful appearance. Dr. Kunal Sayani, one of the top plastic surgeon in Mumbai, a
+leading expert in hair transplant, combines precision, artistry, and cutting-edge techniques to
+deliver natural-looking and lasting results.
         </p>
         <p className="mb-3">
-          A tummy tuck, medically known as abdominoplasty, is a surgical
-          procedure designed to improve the shape and appearance of the abdomen
-          by removing excess skin and fat and tightening the abdominal muscles.
-          This results in a smoother, firmer, and more toned stomach area. It is
-          particularly beneficial for individuals who have experienced
-          significant weight loss, pregnancy, or aging, which can cause
-          stretched skin and weakened muscles. The procedure can be customized
-          to the patient’s needs, ranging from a full tummy tuck addressing the
-          entire abdominal area to a mini tummy tuck focusing on the lower
-          abdomen.
+          Hair transplant surgery is a highly effective and permanent solution for individuals experiencing
+various forms of hair loss, including male pattern baldness, female pattern hair loss, and hair1
+loss due to trauma or scarring. The procedure involves harvesting healthy hair follicles from a
+donor area (typically the back or sides of the head, where hair is genetically resistant to balding)
+and transplanting them2
+to areas of thinning or baldness. Under the expert care of Dr. Kunal
+Sayani, you can achieve natural-looking hair growth that blends seamlessly with your existing
+hair.
+
         </p>
-        <p className="mb-3">
-          Abdominoplasty is not a weight-loss procedure but rather a body
-          contouring solution that addresses aesthetic concerns that diet and
-          exercise alone cannot resolve. Whether it’s restoring a pre-pregnancy
-          figure or achieving a sculpted abdomen, Dr. Kunal Sayani employs
-          state-of-theart methods to ensure optimal outcomes with minimal
-          scarring and downtime. The procedure can be combined with liposuction
-          for enhanced contouring or customized as a mini tummy tuck for
-          patients with less extensive needs.
-        </p>
-        <p className="mb-3">
-          Dr. Kunal Sayani, one of the top plastic surgeon in Mumbai for tummy
-          tuck, performs tummy tuck surgeries tailored to individual goals,
-          ensuring natural-looking results with minimal scarring. The surgery
-          typically involves an incision above the pubic area, muscle
-          tightening, removal of excess fat and skin, and repositioning of the
-          belly button to maintain a natural contour.
-        </p>
+      
        </div>
       </div>
     </section>
