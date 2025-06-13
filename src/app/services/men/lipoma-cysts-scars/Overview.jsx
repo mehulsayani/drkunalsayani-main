1b0c861d0473a5fc6be455471b2007c49ac8d6cf
rajurao <rajurao1107@gmail.com>
2025-06-13 12:14:57 +0530

    commit

--- src/app/services/men/lipoma-cysts-scars/Overview.jsx
+++ src/app/services/men/lipoma-cysts-scars/Overview.jsx
@@ -8,7 +8,7 @@ export default function Overview() {
     <section>
       <div className="bg-white h-full">
         <Image
-          src={"/images/services/men/Lipoma-Cysts-Scars.jpg"}
+        src={"/images/homepage/specialization/hair-transplant.jpg"}
           width={1000}
           height={1000}
           alt=""
