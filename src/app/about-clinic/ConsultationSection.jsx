c56f8ebc18a768e2731d46bfd7e4f50fa7f345a3
rajurao <rajurao1107@gmail.com>
2025-06-16 14:53:19 +0530

    commit

--- src/app/about-clinic/ConsultationSection.jsx
+++ src/app/about-clinic/ConsultationSection.jsx
@@ -2,6 +2,85 @@
 import Image from "next/image";
 
 export default function ConsultationSection() {
+  const submenus = [
+  {
+    title: "Hair",
+    items: [
+      "Hair Transplant",
+      "Hair Restoration PRP GFC Exosomes",
+      "Nano fat injection (SVC) for Hair Growth",
+    ],
+  },
+  {
+    title: "Face",
+    items: [
+      "Rhinoplasty (Nose job)",
+      "Eyelid Surgery (Blepharoplasty)",
+      "Prominent Ear Surgery (Otoplasty)",
+      "Facelift",
+      "Thread Lift Treatment",
+      "Chin Augmentation",
+      "Jawline Contouring",
+      "Lip Lift (Lip job)",
+      "Lipoma-Cysts-Scars",
+    ],
+  },
+  {
+    title: "Breast",
+    items: [
+      "Breast Augmentation",
+      "Breast Lift (mastopexy) with Shaping",
+      "Breast Reduction & Axillary Breasts Excision",
+    ],
+  },
+  {
+    title: "Body",
+    items: [
+      "Tummy Tuck (Abdominoplasty)",
+      "Liposuction",
+      "Buttock Augmentation (BBL)",
+      "Fat Grafting",
+    ],
+  },
+  {
+    title: "Mommy Makeover",
+    items: [
+      "Post-Pregnancy Tummy Tuck (moms)",
+      "Breast Lift & Augmentation after Pregnancy",
+      "Genital Cosmetic Surgeries",
+      "Mommy Makeover Package (custom combination of procedures)",
+    ],
+  },
+  {
+    title: "Non Surgical",
+    items: [
+      "Botox (wrinkles-anti-ageing)",
+      "Dermal Fillers",
+      "Laser Skin Rejuvenation",
+      "Microneedling & PRP for Skin Rejuvenation",
+      "Skin Tightening",
+    ],
+  },
+  {
+    title: "Men",
+    items: [
+      "Gynaecomastia",
+      "Liposuction",
+      "Hair Loss Treatment",
+      "Hair Transplant",
+      "Rhinoplasty (Nose job)",
+      "Eyelid Surgery (Blepharoplasty)",
+      "Prominent Ear Surgery (Otoplasty)",
+      "Facelift",
+      "Thread Lift Treatment",
+      "Chin Augmentation",
+      "Jawline Contouring",
+      "Lip Lift (Lip job)",
+      "Lipoma Cysts Scars",
+    ],
+  },
+];
+
   return (
     <section className="py-16 px-4 w-full md:px-6 bg-[#1AAEBC] text-gray-800 flex justify-center items-center">
       <div className="max-w-[1200px] w-full flex flex-col max-lg:flex-col-reverse lg:flex-row gap-10">
@@ -30,12 +109,19 @@ export default function ConsultationSection() {
                 placeholder="Phone Number"
                 className="border border-gray-300 p-3 rounded-lg w-full bg-white"
               />
-              <select className="border border-gray-300 p-3 rounded-lg w-full bg-white">
-                <option>Choose an Option</option>
-                <option>Skin Care</option>
-                <option>Hair Treatment</option>
-                <option>Body Wellness</option>
-              </select>
+              <select >
+  <option>Choose an Option</option>
+  {submenus.map((menu, idx) => (
+    <optgroup key={idx} label={menu.title}>
+      {menu.items.map((item, itemIdx) => (
+        <option key={itemIdx} value={item}>
+          {item}
+        </option>
+      ))}
+    </optgroup>
+  ))}
+</select>
+
             </div>
             <textarea
               rows="4"
