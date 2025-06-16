3135130d4a5d3d99aa3cdb91c61ef6e1b987a45e
rajurao <rajurao1107@gmail.com>
2025-06-16 21:11:25 +0530

    commit

--- src/app/homepage/hero/Slide1.jsx
+++ src/app/homepage/hero/Slide1.jsx
@@ -15,7 +15,7 @@ export default function Slide1() {
      transition-opacity px-18 duration-700 ease-in-out">
       <div className="flex items-center   max-w-[1200px] max-xl:max-w-[1000px] justify-between  max-lg:flex-col">
         {/* Left Content */}
-        <div className="max-w-5xl w-full   mb-10 md:mb-0 mt-8 max-lg:flex max-lg:justify-center 
+        <div className="max-w-5xl w-full  max-lg:mb-2 mb-10 md:mb-0 mt-8 max-lg:flex max-lg:justify-center 
         max-lg:items-center max-lg:flex-col">
           <h1
             className="text-4xl max-lg:text-3xl  md:text-[40px] max-lg:text-center 
