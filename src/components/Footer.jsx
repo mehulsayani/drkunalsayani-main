e5c656f227e55d5536d28e8b662fa4fe897b144a
rajurao <rajurao1107@gmail.com>
2025-06-19 11:45:54 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -248,7 +248,7 @@ export default function Footer() {
           pracharkarsolutions
         </a>
         {/* Disclaimer Section */}
-      <section className="max-w-4xl text-base mx-auto px-4 pt-5 text-white">
+      <section className="max-w-4xl text-base max-lg:text-xs mx-auto px-4 pt-5 text-white">
        
         <p className="mb-2">
          <span className="text-yellow-400"> Disclaimer:</span> The information presented on this website is intended solely for general
